4735c7a41d391d00c1e49c422104dba15148ca21
Veronika <verkalina00@gmail.com>
2025-06-17 18:07:41 +0500

    УП 1.1

---
+++ zd1_Kalinovskaya/zd1_Kalinovskaya/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd1_Kalinovskaya
+{
+    public class Cat
+    {
+        private string name;//хранение имени кота
+        private double weight;//храниение веса кота
+        public Cat(string CatName)// Конструктор класса Cat
+        {
+            name = CatName;
+        }
+        public Cat(double CatWeight)// Конструктор класса Cat
+        {
+            weight = CatWeight;
+        }
+        public void Meow()//метод для вывода сообщения кота
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!");
+        }
+        public bool SetCatName(string newName)//метод проверки ввода имени кота
+        {
+            foreach (char c in newName)// Проверяем каждый символ в имени
+            {
+                if (!char.IsLetter(c))// Допускаются только буквы
+                {
+                    Console.WriteLine("Имя должно содержать только буквы!");
+                    return false;
+                }
+            }
+            name = newName;// Сохраняем новое имя
+            return true;
+        }
+        public bool SetCatWeight(double newWeight)//метод проверки ввода веса кота
+        {
+            if (newWeight <= 0)// Вес должен быть положительным числом
+            {
+                Console.WriteLine("Вес должен быть положительным числом!");
+                return false;
+            }
+            weight = newWeight;// Сохраняем новый вес
+            return true;
+        }
+        public void PrintInfo()//метод выводит информацию о коте
+        {
+            Console.WriteLine($"\n--- Информация о коте ---");
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Вес: {weight} кг");
+        }
+    }
+}
---
+++ zd1_Kalinovskaya/zd1_Kalinovskaya/Program.cs
@@ -0,0 +1,33 @@
+﻿using zd1_Kalinovskaya;
+string name;
+double weight;
+while (true)
+{
+    Console.Write("Введите имя кота: ");
+    name = Console.ReadLine();
+    if (!string.IsNullOrEmpty(name))
+        break;
+    Console.WriteLine("Имя не может быть пустым!");
+}
+Cat cat = new Cat(name);// Создаем экземпляр класса Cat
+if (!cat.SetCatName(name))
+{
+    return;
+}
+
+while (true)
+{
+    Console.Write($"Введите вес кота {name}: ");
+    string input = Console.ReadLine();
+    if (double.TryParse(input, out weight))
+        break;
+    Console.WriteLine("Вес введите числом");
+}
+Cat catW = new Cat(weight);// Создаем экземпляр класса Cat
+if (!cat.SetCatWeight(weight))
+{
+    return;
+}
+cat.PrintInfo();
+cat.Meow();
+Console.ReadKey();
